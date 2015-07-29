e909018b8c549afb1efd208591f23a3ad96fcc3f
AmericanKulak <eviltygre@gmail.com>
2015-07-28 23:03:06 -0700

    Implement user/identity, wire up filtering properly.

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
+var Identity = require('./lib/identity');
+var PageRenderer = require('./lib/pageRenderer');
 var SearchPipeline = require('./lib/searchPipeline');
 
 function main() {
-  var pagesToShow = SearchPipeline.search('cat');
-  console.log(pagesToShow);
+  var kSearchTerm = 'head';
+  var currentUser, pagesToShow;
+
+  console.log('\r\nSearching for "'+ kSearchTerm + '".\r\n')
+
+  console.log('Player one, no access to resource with search term.');
+  currentUser = Identity.getUserById('PlayerOne');
+  pagesToShow = SearchPipeline.search(kSearchTerm, currentUser);
+  renderPages(pagesToShow, currentUser);
+
+  console.log('\r\n==================\r\n');
+
+  console.log('Player two, specific access to resource with search term.');
+  currentUser = Identity.getUserById('PlayerTwo');
+  pagesToShow = SearchPipeline.search(kSearchTerm, currentUser);
+  renderPages(pagesToShow, currentUser);
+
+  console.log('\r\n==================\r\n');
+
+  console.log('A fate, access to all resources.');
+  currentUser = Identity.getUserById('Fate');
+  pagesToShow = SearchPipeline.search(kSearchTerm, currentUser);
+  renderPages(pagesToShow, currentUser);
+}
+
+function renderPages(pages, user) {
+  for (var i = 0; i < pages.length; i++) {
+    printPage(PageRenderer.renderPage(pages[i], user));
+  };
+}
+
+function printPage(object) {
+  console.log(JSON.stringify(object, ['title', 'sections', 'header', 'paragraphs', 'text'], 2));
 }
 
 main();
