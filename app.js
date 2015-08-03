c5915d6ea8d433e41be0d5faeea36cc96859a1fb
AmericanKulak <eviltygre@gmail.com>
2015-08-03 00:01:21 -0700

    Implement express as the web server framework.

--- app.js
+++ app.js
@@ -1,47 +1,28 @@
-var Identity = require('./lib/identity');
-var PageRenderer = require('./lib/pageRenderer');
-var SearchPipeline = require('./lib/searchPipeline');
-
-function main() {
-  var kSearchTerm = 'head';
-  var currentUser, pagesToShow;
-
-  console.log('\r\nSearching for "'+ kSearchTerm + '".\r\n')
-
-  console.log('Player one, no access to resource with search term.');
-  currentUser = Identity.getUserById('PlayerOne');
-  pagesToShow = SearchPipeline.search(kSearchTerm, currentUser);
-  renderPages(pagesToShow, currentUser);
-
-  console.log('\r\n==================\r\n');
-
-  console.log('Player two, specific access to resource with search term.');
-  currentUser = Identity.getUserById('PlayerTwo');
-  pagesToShow = SearchPipeline.search(kSearchTerm, currentUser);
-  renderPages(pagesToShow, currentUser);
-
-  console.log('\r\n==================\r\n');
-
-  console.log('A fate, access to all resources.');
-  currentUser = Identity.getUserById('Fate');
-  pagesToShow = SearchPipeline.search(kSearchTerm, currentUser);
-  renderPages(pagesToShow, currentUser);
-}
-
-function renderPages(pages, user) {
-  for (var i = 0; i < pages.length; i++) {
-    printPage(PageRenderer.renderPage(pages[i], user));
-  };
-}
-
-function printPage(object) {
-  console.log(JSON.stringify(object, ['title', 'sections', 'header', 'paragraphs', 'text'], 2));
-}
-
-main();
-
-
 // In order to start the application,
-//   Tack on the authorizer (with failure logger)
 //   Tack on the request logger.
+//   Tack on the authorizer (with failure logger)
 //   Tack on the various Routers from subsections.
+
+var app = require('express')();
+var logger = require('./lib/logger').Logger;
+app.set('view engine', 'jade');
+app.set('views', ['./views', './lib/wiki/views']);
+
+// Tack on the authorizers
+
+
+app.use(logger);
+
+app.use('/wiki', require('./lib/wiki'));
+
+app.get('/', function (req, res) {
+  res.logAndRender('basic', {'whee':'Hello World!'});
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('wiki app listening at http://%s:%s', host, port);
+});
+
---
+++ testBed.js
@@ -0,0 +1,41 @@
+var Identity = require('./lib/identity');
+var PageRenderer = require('./lib/wiki/pageRenderer');
+var SearchPipeline = require('./lib/wiki/searchPipeline');
+
+function main() {
+  var kSearchTerm = 'head';
+  var currentUser, pagesToShow;
+
+  console.log('\r\nSearching for "'+ kSearchTerm + '".\r\n')
+
+  console.log('Player one, no access to resource with search term.');
+  currentUser = Identity.getUserById('PlayerOne');
+  pagesToShow = SearchPipeline.search(kSearchTerm, currentUser);
+  renderPages(pagesToShow, currentUser);
+
+  console.log('\r\n==================\r\n');
+
+  console.log('Player two, specific access to resource with search term.');
+  currentUser = Identity.getUserById('PlayerTwo');
+  pagesToShow = SearchPipeline.search(kSearchTerm, currentUser);
+  renderPages(pagesToShow, currentUser);
+
+  console.log('\r\n==================\r\n');
+
+  console.log('A fate, access to all resources.');
+  currentUser = Identity.getUserById('Fate');
+  pagesToShow = SearchPipeline.search(kSearchTerm, currentUser);
+  renderPages(pagesToShow, currentUser);
+}
+
+function renderPages(pages, user) {
+  for (var i = 0; i < pages.length; i++) {
+    printPage(PageRenderer.renderPage(pages[i], user));
+  };
+}
+
+function printPage(object) {
+  console.log(JSON.stringify(object, ['title', 'sections', 'header', 'paragraphs', 'text'], 2));
+}
+
+main();
